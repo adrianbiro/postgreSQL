e0b7fe33f3cc0164c148607f99a4f1dde17be3a2
Adrián Bíro <biroadrian@protonmail.com>
2022-05-06 15:49:37 +0200

    small fixes

--- cap5/cap5.sql
+++ cap5/cap5.sql
@@ -119,4 +119,12 @@ COPY
 FROM '/tmp/blab.csv'
 WITH (FORMAT CSV, HEADER, DELIMITER ':')
 
+COPY (
+	SELECT state_name, births_2019 	
+	FROM us_counties_pop_est_2019
+	ORDER BY births_2019 DESC
+	LIMIT 20
+)
+TO '/tmp/us_top20births.csv'
+WITH (FORMAT CSV, HEADER, QUOTE '"');
 
