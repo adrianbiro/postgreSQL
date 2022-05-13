ff2e439713e95e8d05da1344f85bed6d409014a1
Adrián Bíro <biroadrian@protonmail.com>
2022-05-13 11:43:21 +0200

    small fixes

--- cap12.sql
+++ cap12.sql
@@ -217,4 +217,40 @@ SELECT segment,
 FROM train_rides;
 
 -- Zadanie
+SELECT
+    trip_id,
+    tpep_pickup_datetime,
+    tpep_dropoff_datetime,
+    tpep_dropoff_datetime - tpep_pickup_datetime AS length_of_ride
+FROM nyc_yellow_taxi_trips
+ORDER BY length_of_ride DESC;
+
+SELECT '2100-01-01 00:00:00-05' AT TIME ZONE 'US/Eastern' AS new_york,
+       '2100-01-01 00:00:00-05' AT TIME ZONE 'Europe/London' AS london,
+       '2100-01-01 00:00:00-05' AT TIME ZONE 'Africa/Johannesburg' AS johannesburg,
+       '2100-01-01 00:00:00-05' AT TIME ZONE 'Europe/Moscow' AS moscow,
+       '2100-01-01 00:00:00-05' AT TIME ZONE 'Australia/Melbourne' AS melbourne;
+
+SELECT
+    round(
+          corr(total_amount, (
+              date_part('epoch', tpep_dropoff_datetime) -
+              date_part('epoch', tpep_pickup_datetime)
+                ))::numeric, 2
+          ) AS amount_time_corr,
+    round(
+        regr_r2(total_amount, (
+              date_part('epoch', tpep_dropoff_datetime) -
+              date_part('epoch', tpep_pickup_datetime)
+        ))::numeric, 2
+    ) AS amount_time_r2,
+    round(
+          corr(total_amount, trip_distance)::numeric, 2
+          ) AS amount_distance_corr,
+    round(
+        regr_r2(total_amount, trip_distance)::numeric, 2
+    ) AS amount_distance_r2
+FROM nyc_yellow_taxi_trips
+WHERE tpep_dropoff_datetime - tpep_pickup_datetime <= '3 hours'::interval;
+
 
